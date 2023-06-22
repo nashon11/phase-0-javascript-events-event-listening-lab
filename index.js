7fecb298ae93a97348ca5afd9d71e1f70ffaad9c
NASH <nashon.mwenda@student.moringaschool.com>
2023-06-22 15:29:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+// index.js
 function addingEventListener() {
-}
+    const button = document.querySelector('#myButton');
+      return true; // Indicate that the event listener was successfully added
+    }
+  // Make sure to replace '#myButton' with the actual selector for your button element
+  
